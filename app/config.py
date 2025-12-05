b983d321ebc5a27343c894770300eda7b86d5315
InvestMal <manmkeen@gmail.com>
2025-12-05 20:31:06 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -3,3 +3,5 @@ import os
 TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
 WEBHOOK_URL = os.environ.get("WEBHOOK_URL")
 PORT = int(os.environ.get("PORT", 10000))
+
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
